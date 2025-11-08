cd67d4445dd410f1238a68ee3715d43b467cb69c
osama-bilal <63829819+osama-bilal@users.noreply.github.com>
2025-11-08 23:18:03 +0300

    Update parties.dart
    split app parties every one to individually Set variable

--- lib/controllers/provider/parties.dart
+++ lib/controllers/provider/parties.dart
@@ -8,8 +8,21 @@ import 'package:ponit_of_sales/services/general_services.dart';
 
 class AppParties extends ChangeNotifier {
   Set<ViewParty> parties = {};
-
-  void addList<T>(List<ViewParty<T>> l) {
+  Set<ViewParty<Customer>> customers = {};
+  Set<ViewParty<Supplier>> suppliers = {};
+Set<ViewParty<Employee>> employees = {};
+Set<ViewParty<Group>> groups = {};
+Set<ViewParty<Permission>> permissions = {};
+Set<ViewParty<ContentType>> contentTypes = {};
+final tempService = GeneralService<ViewParty<Object>>(
+        endpoint: "",
+        fromMap: ViewParty<Object>.fromMap,
+        toMap: (o) => o.toMap(),
+      );
+void save(){
+notifyListeners();
+}
+  /* void addList<T>(List<ViewParty<T>> l) {
     parties.addAll(l);
     notifyListeners();
   }
@@ -38,40 +51,78 @@ class AppParties extends ChangeNotifier {
   List<ViewParty> get<T>() {
     return parties.where((p) => p.type == T).toList();
   }
+*/
+Future<void> fetchCustomers() async {
+customers.clear();
+customers.addAll(await load<Customer>("/parties/customers/"));
+notifyListeners();
+}
+Future<void> fetchSuppliers() async {
+suppliers.clear();
+suppliers.addAll(await load<Supplier>("/parties/suppliers/"));
+notifyListeners();
+}
+Future<void> fetchEmployees() async {
+employees.clear();
+employees.addAll(await load<Employee>("/parties/employees/"));
+notifyListeners();
+}
+Future<void> fetchGroups() async {
+groups.clear{};
+groups.addAll(await load<Group>("/parties/groups/"));
+notifyListeners();
+}
+Future<void> fetchPermissions() async {
+permissions.clear();
+permissions.addAll(await load<Permission>("/parties/permissions/"));
+notifyListeners();
+}
+Future<void> fetchCT() async {
+contentTypes.clear();
+contentTypes.addAll(await load<ContentType>("/parties/contenttypes/"));
+notifyListeners();
+}
 
-  Future<void> getReady() async {
-    Future<void> load<T>(String path) async {
-      final tempService = GeneralService<ViewParty>(
-        endpoint: "",
-        fromMap: ViewParty.fromMap,
-        toMap: (o) => o.toMap(),
-      );
-      tempService.endpoint = path;
+
+Future<List<ViewParty<T>>> load<T>(String path) async {
+       
+      this.tempService.endpoint = "/parties/$path/";
       final raw = await tempService.fetchList();
       final typed = raw.map((e) {
         // نفترض أن ViewParty.fromMap يعرف يبني ViewParty<T>
-        return ViewParty<T>.fromMap(e.toMap());
+return ViewParty<T>(id: e.id, name: e.name);
+      //  return ViewParty<T>.fromMap(e.toMap());
       }).toList();
-      addList<T>(typed);
+return typed;
+  //    addList<T>(typed);
     }
 
+  Future<void> getReady() async {
+    
+
     try {
-      await load<Customer>("/parties/customers/");
+     customers.clear();
+customers.addAll(await load<Customer>("customers"));
     } catch (_) {}
     try {
-      await load<Supplier>("/parties/suppliers/");
+     suppliers.clear();
+suppliers.addAll(await load<Supplier>("suppliers"));
     } catch (_) {}
     try {
-      await load<Employee>("/parties/employees/");
+      employees.clear();
+employees.addAll(await load<Employee>("employees"));
     } catch (_) {}
     try {
-      await load<Group>("/parties/groups/");
+      groups.clear{};
+groups.addAll(await load<Group>("groups"));
     } catch (_) {}
     try {
-      await load<Permission>("/parties/permissions/");
+      permissions.clear();
+permissions.addAll(await load<Permission>("permissions"));
     } catch (_) {}
     try {
-      await load<ContentType>("/parties/contenttypes/");
+      contentTypes.clear();
+contentTypes.addAll(await load<ContentType>("contenttypes"));
     } catch (_) {}
   }
 }
